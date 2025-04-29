9aabe91abd335270366d8fb3bd8bce1a453959fa
Elyas Damenu <elyasdamenu@gmail.com>
2025-04-29 20:09:09 +0100

    finished the top section below the navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
     <div class="space-y-10">
+        <section class="text-center">
+            <h1 class="font-bold text-4xl mt-10">Let's Find You A Great Job</h1>
+            <form action="">
+                <input class="border border-white/10 rounded-xl bg-white/5 px-5 py-4 w-full max-w-xl mt-3" type="text"
+                    placeholder="I'm looking for...">
+            </form>
+        </section>
         <section class="w-full mt-10">
             <x-s-heading>Featured Jobs</x-s-heading>
             <div class="grid lg:grid-cols-3 gap-8 w-full mt-6">
