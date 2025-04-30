616845a7a591313be4473168fa49f139190453cb
Elyas Damenu <elyasdamenu@gmail.com>
2025-04-30 12:11:24 +0100

    installed pest and finished TDD

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -9,6 +9,10 @@ class JobTest extends TestCase
     /**
      * A basic unit test example.
      */
+    //AAA
+    //first : arrange ; the world in order to run the test
+    //second: act ; perform an action
+    //third : assert ; what do you expect to happen
     public function test_example(): void
     {
         $this->assertTrue(true);
